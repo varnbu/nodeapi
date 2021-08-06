c11da5fb00b40ecd93255a701093f8d04d9d47f5
varn <varnbu@gmail.com>
2021-08-06 17:19:12 +0800

    label 显示问题

--- woo/echarts/utils.js
+++ woo/echarts/utils.js
@@ -1,39 +1,75 @@
-function createGrid (it, index) {
+function createGrid (it, index, DEVIDE_VALUE,LIST_lENGTH) {
     return {
         show: false,
-        top: index * 100,
-        height:100,
+        top: index * 100 + 10,
+        height: 100,
         left: 80,
         right: 20
     }
 }
 
-function createXAes (it, index) {
+function createXAes (it, index, DEVIDE_VALUE,LIST_lENGTH) {
     return {
         type: 'value',
         show: true,
         gridIndex: index,
-        minorTick:{
+        minorTick: {
             show: true,
         },
-        axisLine:{
-            show:index===6
+        axisLabel: {
+            show: index === LIST_lENGTH-1
         },
-        splitLine:{
-            show:true,
+        splitLine: {
+            show: true,
             lineStyle: {
                 // 使用深浅的间隔色
                 color: '#999',
-                width:1,
+                width: 1,
                 type: 'solid'
             }
         },
-        minorSplitLine:{
-            show:true,
+        minorSplitLine: {
+            show: true,
             lineStyle: {
                 // 使用深浅的间隔色
                 color: '#ddd',
-                width:1,
+                width: 1,
+                type: 'solid'
+            }
+        }
+    }
+}
+
+function createYAes (it, index, DEVIDE_VALUE,LIST_lENGTH) {
+    return {
+        type: 'value',
+        show: true,
+        gridIndex: index,
+        minorTick: {
+            show: true,
+        },
+        axisLabel: {
+            showMinLabel:true,
+            showMaxLabel:index===0,
+        },
+        splitLine: {
+            show: true,
+            lineStyle: {
+                // 使用深浅的间隔色
+                color: '#999',
+                width: 1,
+                type: 'solid'
+            }
+        },
+        min: 0,
+        max: (Number(it.RangeMax) / DEVIDE_VALUE).toFixed(0
+        ),
+        minorSplitLine: {
+            show: true,
+            lineStyle: {
+                // 使用深浅的间隔色
+                color: '#ddd',
+                width: 1,
                 type: 'solid'
 
             }
@@ -41,37 +77,7 @@ function createXAes (it, index) {
     }
 }
 
-function createYAes (it, index) {
-    return {
-        type: 'value',
-        show: true,
-        gridIndex: index,
-        minorTick:{
-            show: true,
-        },
-        splitLine:{
-            show:true,
-            lineStyle: {
-                // 使用深浅的间隔色
-                color: '#999',
-                width:1,
-                type: 'solid'
-            }
-        },
-        minorSplitLine:{
-            show:true,
-            lineStyle: {
-                // 使用深浅的间隔色
-                color: '#ddd',
-                width:1,
-                type: 'solid'
-
-            }
-        }
-    }
-}
-
-function createSeries (it, index,data) {
+function createSeries (it, index, data, DEVIDE_VALUE,LIST_lENGTH) {
     return {
         name: it.LeadType,
         type: 'line',
@@ -80,17 +86,17 @@ function createSeries (it, index,data) {
         data: data,
         showSymbol: false,
         itemStyle: {
-            color:'#333'
+            color: '#333'
         }
     }
 }
 
-function createTitle (it, index) {
+function createTitle (it, index, DEVIDE_VALUE,LIST_lENGTH) {
     return {
         textAlign: 'center',
         text: it.LeadType,
-        left:12,
-        top: index * 100+50-12,
+        left: 12,
+        top: index * 100 + 50 - 12,
         textStyle: {
             fontSize: 12,
             fontWeight: 'normal'
